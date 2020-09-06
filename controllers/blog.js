99330a3e3b91cb4564f75dd9bf6d901f44d539a3
ikhvjs <ikhvjs@gmail.com>
2020-09-06 18:05:27 +0200

    add Create Blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -20,23 +20,23 @@ const handleBlogGet =(req,res,db)=>{
 
 
 
-// const handleBlogTagGet = (req,res,db) =>{
-// 	const { blogPath } = req.params;
+const handleBlogTagGetByBlogPath = (req,res,db) =>{
+	const { blogPath } = req.params;
 
-// 	db.select('tt.tag_name'
-// 			,'tt.tag_id')
-// 	.from('tb_tag as tt')
-// 	.join('tb_tag_link as ttl', function(){
-// 		this.on('ttl.tag_id','=','tt.tag_id')
-// 	})
-// 	.join('tb_blog as tb', function(){
-// 		this.on('ttl.tag_group_id','=','tb.tag_group_id')
-// 		.andOn('tb.blog_path','=',db.raw('?',[blogPath]))
-// 	})
-// 	.then(data=>res.json(data))
-// 	.catch(err => res.status(400).json('error getting tag'));
+	db.select('tt.tag_name'
+			,'tt.tag_id')
+	.from('tb_tag as tt')
+	.join('tb_tag_link as ttl', function(){
+		this.on('ttl.tag_id','=','tt.tag_id')
+	})
+	.join('tb_blog as tb', function(){
+		this.on('ttl.tag_group_id','=','tb.tag_group_id')
+		.andOn('tb.blog_path','=',db.raw('?',[blogPath]))
+	})
+	.then(data=>res.json(data))
+	.catch(err => res.status(400).json('error getting tag'));
 
-// }
+}
 
 const handleBlogCategoryGet = (req,res,db) =>{
 	db.orderBy('blog_category_id','desc')
@@ -76,12 +76,76 @@ const handleBlogTagSearch=(req,res,db)=>{
 	.catch(err => res.status(400).json('error search blog category'));
 }
 
+const handleBlogPost =(req,res,db)=>{
+	const{newBlog} = req.body;
+	// console.log('newBlog',newBlog);
+
+	db.transaction(trx => {
+      trx.select('menu_id')
+      .from('tb_menu')
+      .where('menu_path','=',newBlog.menu_path)
+      // .then(menu_id => console.log('menu_id',menu_id[0].menu_id))
+      .then(menu_id=>{
+      		// console.log('menu_id',menu_id[0].menu_id);
+      	 	return trx('tb_blog')
+      	 		.returning('tag_group_id')
+      			.insert({
+	      			menu_id: menu_id[0].menu_id,
+	      			blog_title: newBlog.blog_title,
+	      			blog_desc: newBlog.blog_desc,
+	      			blog_path: newBlog.blog_path,
+	      			blog_category_id: newBlog.blog_category_id,
+	      			seq: newBlog.seq,
+	      			blog_content: newBlog.blog_content,
+	      			created_date:new Date(),
+				    created_by:'testingUser1',
+				    last_updated_date:new Date(),
+				    last_updated_by:'testingUser1'
+	      		})
+	      		.then(tag_group_id=>{
+	      			// console.log('tag_group_id',tag_group_id[0]);
+      				
+      				const promises = newBlog.tags.map((tag,index)=>{
+      					// console.log('tag',tag);
+      					return trx('tb_tag_link')
+      					.returning('tag_link_id')
+	      				.insert({
+	      					tag_group_id: tag_group_id[0],
+	      					tag_id: tag.tag_id,
+	      					created_date:new Date(),
+						    created_by:'testingUser1',
+						    last_updated_date:new Date(),
+						    last_updated_by:'testingUser1'
+	      				})
+	      				.then(tag_link_id => {
+				            // console.log('tag_link_id',tag_link_id[0]);
+				         	return tag_link_id[0];
+				         })	
+	      				
+      				})
+      				return Promise.all(promises);
+      			})
+      			.then(data=>{
+      				// console.log('data',data);
+					res.json(data);
+				})
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+
+    })
+    .catch(err => res.status(400).json(err))
+
+
+}
+
 
 module.exports = {
   handleBlogGet,
-  // handleBlogTagGet,
+  handleBlogTagGetByBlogPath,
   handleBlogCategoryGet,
   handleBlogCategorySearch,
   handleBlogTagGet,
-  handleBlogTagSearch
+  handleBlogTagSearch,
+  handleBlogPost
 }
