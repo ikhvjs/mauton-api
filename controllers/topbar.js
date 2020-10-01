1ac8c2b1022568dbb599097ee791cc3925e8d164
ikhvjs <ikhvjs@gmail.com>
2020-10-01 16:09:03 +0200

    update connection

--- controllers/topbar.js
+++ controllers/topbar.js
@@ -3,7 +3,7 @@ const handleTopbarGet = (req,res,db) => {
 	.from('tb_menu')
 	.where({menu_level:1})
 	.then(menu=>res.json(menu))
-	.catch(err => res.status(400).json('error getting menu')); 
+	.catch(err => res.status(400).json('error getting topbar menu')); 
 }
 
 module.exports = {
