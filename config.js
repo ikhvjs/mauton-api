4cf0b2dd1fb468afd3010865cd822f6439b91970
ikhvjs <ikhvjs@gmail.com>
2020-12-09 12:24:05 +0100

    add config.js

---
+++ config.js
@@ -0,0 +1,39 @@
+const env = process.env.NODE_ENV || "dev"
+
+const configs = {
+    base: {
+        env,
+        tokenExpiredTime : 2000, //in seconds
+		secret: "gklskdfj@KJFLGksjgPosj&hgjs1kjfj3SG9491f",
+		captchaSecret: "6LcqXdgZAAAAAD3WH0cQ8Xz8q0NKU96zbYgYTxVa",
+    },
+    dev: {
+    	port:3001,
+        database:{
+		  	client: 'pg',
+		  	connection: {
+			    host : '127.0.0.1',
+			    user : 'postgres',
+			    password : '123a123a',
+			    database : 'books'
+		  	}
+		}
+
+    },
+    prod: {
+    	port:process.env.PORT,
+        database:{
+		  	client: 'pg',
+		  	connection: {
+		    	connectionString : process.env.DATABASE_URL,
+			    ssl: {
+			    	rejectUnauthorized: false
+			  	}
+  			}
+		}
+    }
+};
+
+const config = Object.assign(configs.base, configs[env]);
+
+module.exports= config;
